e8c3ee85c5e38e0bcd302dc09eb86aefd1406a12
Kafrinah <19826373387@163.com>
2026-06-07 11:58:07 +0800

    Refactor Achievements.vue template and styles修bug

--- src/views/Achievements.vue
+++ src/views/Achievements.vue
@@ -145,7 +145,7 @@
 
 .achievements-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   gap: 16px;
 }
 
